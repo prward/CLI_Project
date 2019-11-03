df62e46261deaa813dd30c8bd300bfa9ce393776
Patrick Ward <patrick.r.ward@verizon.net>
2019-11-03 14:27:51 +0000

    moved movies into cli

--- lib/CLI_Project/cli.rb
+++ lib/CLI_Project/cli.rb
@@ -1,10 +1,56 @@
 class CLIProject::CLI
   #command interface when running ./bin/CLI_Project.rb
+  attr_reader :name, :showtime, :url
+   @@action = []
+   @@horror = []
+   @@drama = []
+  
   def call
     list_genres
     menu
     quit
   end
+  
+  def action
+    @@action
+  end
+  
+  def horror
+    @@horror
+  end
+  
+  def drama
+    @@drama
+  end
+  
+   def self.movie_1(name, showtime, url)
+     @name = "Terminator: Dark Fate"
+     @showtime = "12:40pm, 3:50pm, 7:00pm, 10:10pm"
+     @url = "https://www.regmovies.com/movies/terminator-dark-fate/ho00009832#/buy-tickets-by-film-for-cinemas?at=2019-11-02&for-movie=ho00009832&view-mode=list"
+     @@action << self.movie_1
+   end
+   
+   def self.movie_2(name, showtime, url)
+     @name = "Gemini Man"
+     @showtime = "12:45pm, 3:45pm, 7:45pm, 11:10pm"
+     @url = "https://www.regmovies.com/movies/gemini-man/ho00008476#/buy-tickets-by-film-for-cinemas?at=2019-11-02&for-movie=ho00008476&view-mode=list"
+     @@action << self.movie_2
+   end
+   
+   def self.movie_3(name, showtime, url)
+     @name = "Doctor Sleep"
+     @showtime = "6:00pm, 10:00pm"
+     @url = "https://www.regmovies.com/movies/doctor-sleep/ho00009866#/buy-tickets-by-film-for-cinemas?at=2019-11-07&for-movie=ho00009866&view-mode=list"
+     @@horror << self.movie_3
+   end
+   
+   def self.movie_4(name, showtime, url)
+     @name = "Parasite"
+     @showtime = "1:10pm, 4:10pm, 7:10pm, 10:10pm"
+     @url = "https://www.regmovies.com/movies/parasite/ho00009612#/buy-tickets-by-film-for-cinemas?at=2019-11-02&for-movie=ho00009612&view-mode=list"
+     @@drama << self.movie_4
+   end
+
   
   def list_genres
     puts "Welcome! Please select a genre of movie:"
@@ -15,10 +61,6 @@ class CLIProject::CLI
     DOC
   end
   
-  def movies
-    @movies = CLIProject::Movie
-  end
-  
   def menu
     puts "Type number of the genre you are looking for, back to return to menu or exit to quit:"
     input = nil
@@ -27,14 +69,16 @@ class CLIProject::CLI
       case input
       when "1"
         puts "Action movies currently playing:"
-        movies
+        action
       
       when "2"
         puts "Horror movies currently playing:"
-        @horror
+        horror
+      
       when "3"
         puts "Drama movies currently playing:"
-        @drama
+        drama
+      
       when "back"
         list_genres
         
@@ -50,7 +94,10 @@ class CLIProject::CLI
 end
 
 
-
+  
+   
+   
+  
 
 
 
--- lib/CLI_Project/movie.rb
+++ lib/CLI_Project/movie.rb
@@ -1,28 +1,37 @@
 class CLIProject::Movie
    #currently showing movies and maybe the next show time
-  def self.action
-    @action = [movie_1, movie_2]
-    movie_1.name = "Terminator: Dark Fate"
-    movie_1.showtime = "12:40pm, 3:50pm, 7:00pm, 10:10pm"
-    movie_1.url = "https://www.regmovies.com/movies/terminator-dark-fate/ho00009832#/buy-tickets-by-film-for-cinemas?at=2019-11-02&for-movie=ho00009832&view-mode=list"
-    
-    movie_2.name = "Gemini Man"
-    movie_2.showtime = "12:45pm, 3:45pm, 7:45pm, 11:10pm"
-    movie_2.url = "https://www.regmovies.com/movies/gemini-man/ho00008476#/buy-tickets-by-film-for-cinemas?at=2019-11-02&for-movie=ho00008476&view-mode=list"
+   attr_reader :name, :showtime, :url
+  
+   @@action = []
+   @@horror = []
+   @@drama = []
+   
+   def self.movie_1(name, showtime, url)
+     @name = "Terminator: Dark Fate"
+     @showtime = "12:40pm, 3:50pm, 7:00pm, 10:10pm"
+     @url = "https://www.regmovies.com/movies/terminator-dark-fate/ho00009832#/buy-tickets-by-film-for-cinemas?at=2019-11-02&for-movie=ho00009832&view-mode=list"
+     @@action << self.movie_1
    end
    
-   def self.horror
-     @horror = [movie_3]
-     movie_3.name = "Doctor Sleep"
-     movie_3.showtime = "6:00pm, 10:00pm"
-     movie_3.url = "https://www.regmovies.com/movies/doctor-sleep/ho00009866#/buy-tickets-by-film-for-cinemas?at=2019-11-07&for-movie=ho00009866&view-mode=list"
+   def self.movie_2(name, showtime, url)
+     @name = "Gemini Man"
+     @showtime = "12:45pm, 3:45pm, 7:45pm, 11:10pm"
+     @url = "https://www.regmovies.com/movies/gemini-man/ho00008476#/buy-tickets-by-film-for-cinemas?at=2019-11-02&for-movie=ho00008476&view-mode=list"
+     @@action << self.movie_2
    end
    
-   def self.drama
-     @drama = [movie_4]
-     movie_4.name = "Parasite"
-     movie_4.showtime = "1:10pm, 4:10pm, 7:10pm, 10:10pm"
-     movie_4.url = "https://www.regmovies.com/movies/parasite/ho00009612#/buy-tickets-by-film-for-cinemas?at=2019-11-02&for-movie=ho00009612&view-mode=list"
+   def self.movie_3(name, showtime, url)
+     @name = "Doctor Sleep"
+     @showtime = "6:00pm, 10:00pm"
+     @url = "https://www.regmovies.com/movies/doctor-sleep/ho00009866#/buy-tickets-by-film-for-cinemas?at=2019-11-07&for-movie=ho00009866&view-mode=list"
+     @@horror << self.movie_3
+   end
+   
+   def self.movie_4(name, showtime, url)
+     @name = "Parasite"
+     @showtime = "1:10pm, 4:10pm, 7:10pm, 10:10pm"
+     @url = "https://www.regmovies.com/movies/parasite/ho00009612#/buy-tickets-by-film-for-cinemas?at=2019-11-02&for-movie=ho00009612&view-mode=list"
+     @@drama << self.movie_4
    end
 end
 
